2dc1c43e29796f051ab7975b4ddf78a89dac4ee1
Yoel Martin <yoelmartinfornieles@gmail.com>
2021-10-23 18:00:12 +0200

    feat: fetched data from a fakeApi

--- pages/article/[id]/index.js
+++ pages/article/[id]/index.js
@@ -1,10 +1,78 @@
+/* import { useRouter } from "next/router"
+ */
+import {server} from '../../../config/index'
+import Link from "next/link"
 
-const article = () => {
+const article = ({article}) => {
+/* 	const router = useRouter()
+	const {id} = router.query */
 	return (
-		<div>
-			this is an article
-		</div>
+		<>
+			<h1>{article.title}</h1>
+			<p>{article.body}</p>
+			<br />
+			<Link href='/'>Go Back</Link>
+		</>
 	)
 }
+
+export const getStaticProps = async (context) => {
+	const result = await fetch(`${server}/api/articles/${context.params.id}`)
+	const article = await result.json ()
+	return { 
+		props: {
+			article
+		}
+	}
+}
+
+export const getStaticPaths = async () => {
+	const result = await fetch(`${server}/api/articles`)
+	const articles = await result.json ()
+
+	const ids = articles.map (article => article.id)
+
+	const paths = ids.map (id => ({params: {id: id.toString()}}))
+
+	return {
+		paths,
+		fallback: false 
+	}
+}
+
+/* export const getServerSideProps = async (context) => {
+	const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+	const article = await result.json ()
+	return { 
+		props: {
+			article
+		}
+	}
+} */
+
+/* export const getStaticProps = async (context) => {
+	const result = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+	const article = await result.json ()
+	return { 
+		props: {
+			article
+		}
+	}
+}
+
+export const getStaticPaths = async () => {
+	const result = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+	const articles = await result.json ()
+
+	const ids = articles.map (article => article.id)
+
+	const paths = ids.map (id => ({params: {id: id.toString()}}))
+
+	return {
+		paths,
+		fallback: false 
+	}
+} */
+
 
 export default article
